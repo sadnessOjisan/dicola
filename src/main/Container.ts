830f8c2f31a80ad512d627836f22c39830e00614
sadnessOjisan <sadness.ojisan@gmail.com>
2019-12-02 21:50:24 +0900

    delete needless code (#7)
    * update: minimize
    
    * update: ver and readme

--- src/main/Container.ts
+++ src/main/Container.ts
@@ -23,54 +23,20 @@ class Container {
   }
 
   public resolve(ctor: constructor<any>) {
-    console.log("this.data", this.data);
     this.resolveInstance(ctor);
     const dependantClasses = this.data.get(ctor);
-    if (dependantClasses) {
-      let isInstantbale = false;
-      dependantClasses.forEach(cls => {
-        if (this.context.get(cls)) {
-          isInstantbale = true;
-        }
-      });
-
-      if (isInstantbale) {
-        const instances = dependantClasses.map(cls => this.context.get(cls));
-        return new ctor(...instances);
-      }
-
-      dependantClasses.forEach(cls => {
-        const instance = this.context.get(cls);
-        if (instance) {
-          return new ctor(instance);
-        } else {
-          this.resolveInstance(cls);
-        }
-      });
-
-      const arg = this.context.get(ctor);
-      return arg;
-    } else {
-      this.context.set(ctor, new ctor());
-    }
+    if (!dependantClasses) return;
+    const instances = dependantClasses.map(cls => this.context.get(cls));
+    return new ctor(...instances);
   }
 
   private resolveInstance(cls: any) {
-    const instanceCache = this.context.get(cls);
-    if (instanceCache) {
-      return this.resolve(cls);
-    } else {
-      const depends = this.data.get(cls);
-      if (depends) {
-        const dependInstances = depends.map(d => new d());
-        this.resolve(depends[0]);
-        const instance = new cls(...dependInstances);
-        this.context.set(cls, instance);
-      } else {
-        const instance = new cls();
-        this.context.set(cls, instance);
-      }
-    }
+    const depends = this.data.get(cls);
+    if (!depends) return;
+    const dependInstances = depends.map(d => new d());
+    this.resolve(depends[0]);
+    const instance = new cls(...dependInstances);
+    this.context.set(cls, instance);
   }
 
   public register(constructorToken: any, depend: any) {
