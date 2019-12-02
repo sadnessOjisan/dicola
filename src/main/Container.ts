99fec5e5f95c8e25004f31efd0bf9d501122d2f2
sadnessOjisan <sadness.ojisan@gmail.com>
2019-12-03 01:57:07 +0900

    fix resolve bug (#8)
    * wip
    
    * fix container

--- src/main/Container.ts
+++ src/main/Container.ts
@@ -8,13 +8,14 @@ class Container {
 
   // key is constructor
   data: Map<constructor<any>, constructor<any>[]>;
-  context: Map<constructor<any>, InstanceType<constructor<any>>>;
+  context: Map<constructor<any>, constructor<any>>;
 
   private constructor() {
     this.data = new Map<constructor<any>, constructor<any>[]>();
     this.context = new Map<any, InstanceType<constructor<any>>>();
   }
 
+  // this container is singleton.
   static getInstance() {
     if (!Container.instance) {
       Container.instance = new Container();
@@ -22,7 +23,13 @@ class Container {
     return Container.instance;
   }
 
+  // resolve dependency
   public resolve(ctor: constructor<any>) {
+    const dependantClasses2 = this.data.get(ctor);
+    if (dependantClasses2 && dependantClasses2.length > 0) {
+      const instance = new ctor();
+      this.context.set(ctor, instance);
+    }
     this.resolveInstance(ctor);
     const dependantClasses = this.data.get(ctor);
     if (!dependantClasses) return;
@@ -30,15 +37,24 @@ class Container {
     return new ctor(...instances);
   }
 
-  private resolveInstance(cls: any) {
-    const depends = this.data.get(cls);
-    if (!depends) return;
-    const dependInstances = depends.map(d => new d());
+  // take a instance
+  private resolveInstance(ctor: any) {
+    const depends = this.data.get(ctor);
+    if (!depends) {
+      const i = new ctor();
+      this.context.set(ctor, i);
+      return;
+    }
     this.resolve(depends[0]);
-    const instance = new cls(...dependInstances);
-    this.context.set(cls, instance);
+    const dependInstances = depends.map(d => {
+      return this.context.get(d);
+    });
+
+    const instance = new ctor(...dependInstances);
+    this.context.set(ctor, instance);
   }
 
+  // register dependency
   public register(constructorToken: any, depend: any) {
     this.data.set(constructorToken, depend);
   }
